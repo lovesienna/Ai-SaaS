0fa237d2258b444d31f2ab3212a9fb169226a811
sienna <siennahewer@gmail.com>
2023-07-28 21:26:23 +0900

    Feat: 기본 레이아웃 셋팅 및 사이드바 완성 메인 페이지 퍼블리싱 완성

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
+
+const MobileSidebar = () => {
+  //하이드레이션 에러 방지 코드
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
